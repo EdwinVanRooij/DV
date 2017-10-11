a2952ca618c9e5537f72659e4284aed2b2618fa8
Edwin <edwinvrooij@gmail.com>
2017-10-11 20:44:30 +0200

    by quarter now, script and csv

--- 02/quarterly-post-counter.py
+++ 02/quarterly-post-counter.py
@@ -7,12 +7,22 @@ with open('../data.csv', 'r') as f:
 final_dict = dict()
 
 for item in your_list[1:]:
-    key = item[1][0:4]
+    year = item[1][0:4]
+    month = item[1][5:7]
+    if 1 <= int(month) <= 3:
+        quarter = 1
+    elif 4 <= int(month) <= 6:
+        quarter = 2
+    elif 4 <= int(month) <= 6:
+        quarter = 3
+    else:
+        quarter = 4
+    key = year + '-' + str(quarter)
     final_dict[key] = final_dict.get(key, 0) + 1
 
 print(final_dict)
 
-with open('../posts-by-year.csv', 'w', newline='') as csvfile:
+with open('../posts-by-quarter.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',',
                         quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
