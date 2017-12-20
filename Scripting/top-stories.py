df53cb954df11d6e2d0ca146aef3dfcd627fd89a
Edwin van Rooij <edwinvrooij@gmail.com>
2017-12-20 20:23:45 +0100

    finished top stories

---
+++ Scripting/top-stories.py
@@ -0,0 +1,14 @@
+# Get the top n stories by word x
+import pandas as pd
+
+pd.options.display.max_colwidth = 200
+
+stories = 5
+word = 'china'
+
+df = pd.read_csv('../data/data/data.csv', sep=',', low_memory=False, encoding = 'ISO-8859-1')
+df = df.drop(labels=['subreddit', 'over_18', 'time_created', 'down_votes'], axis=1)
+df = df[df['title'].str.contains(word)]
+df = df.sort_values(by=['up_votes'], ascending=False)
+
+df[0:stories].to_csv('top-{}-stories-{}.csv'.format(stories, word), sep='\t', encoding='utf-8')
