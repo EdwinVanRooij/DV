46339d648995a0c4794ce43338fcb8efc0bd29a1
Edwin van Rooij <edwinvrooij@gmail.com>
2017-12-20 20:40:37 +0100

    finished

--- Scripting/top-stories.py
+++ Scripting/top-stories.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 pd.options.display.max_colwidth = 5000
 
-stories = 5
+stories = 10
 word = 'china'
 
 df = pd.read_csv('../data/data/data.csv', sep=',', low_memory=False, encoding = 'ISO-8859-1')
@@ -11,4 +11,4 @@ df = df.drop(labels=['subreddit', 'over_18', 'time_created', 'down_votes'], axis
 df = df[df['title'].str.contains(word)]
 df = df.sort_values(by=['up_votes'], ascending=False)
 
-df[0:stories].to_csv('../data/top-{}-stories-{}.csv'.format(stories, word), sep=',', encoding='ISO-8859-1')
+df[0:stories].to_csv('../data/top-{}-stories-{}.csv'.format(stories, word), sep=',', encoding='utf-8')
